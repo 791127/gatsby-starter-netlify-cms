2e36ea62f12b3d1c6b1e5c179cee0edb91f31d74
791127 <yachiyo.bear@gmail.com>
2020-08-08 20:22:43 +0900

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,7 +26,7 @@ export const BlogPostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            //<p>{description}</p>
+            <h2>{description}</h2>
             <p>{memo}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
